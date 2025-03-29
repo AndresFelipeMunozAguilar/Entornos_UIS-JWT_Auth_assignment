c4c7a475c23e3416ba5e2a8f7038f50d34ed405f
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-03-29 17:46:12 -0500

    añadir estilos a los componentes de la pagina, de la siguiente manera:
    hacer rotar el logo de react infinitamente
    
    cambiar el tamaño de letra del texto
    
    colorear el header y centrar sus elementos

--- codigo_fuente/frontend/src/components/Header.jsx
+++ codigo_fuente/frontend/src/components/Header.jsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 
 export default function Header(props) {
     return (
-        <header>
-            <img src={props.logoSrc} alt="logo" height="500px" widht="500px" />
-            <h1>{props.pageTitle}</h1>
+        <header className='App-header'>
+            <img src={props.logoSrc} alt="logo" className='App-logo' />
+            <h1 className="App-title">{props.pageTitle}</h1>
         </header>
     )
 }

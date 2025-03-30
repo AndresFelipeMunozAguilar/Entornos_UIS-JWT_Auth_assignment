a67868f5e8a2bea68edf2966a1c1816001f00f54
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-03-30 12:52:02 -0500

    crear logica para mostrar contenido basado en verificar si el usuario inicio sesión

--- codigo_fuente/frontend/src/components/AppContent.jsx
+++ codigo_fuente/frontend/src/components/AppContent.jsx
@@ -2,14 +2,79 @@ import * as React from 'react'
 import WelcomeContent from './WelcomeContent'
 import AuthContent from './AuthContent'
 import LoginForm from './LoginForm'
+import { request } from '../axios_helper'
 
 export default class AppContent extends React.Component {
+
+  constructor(props) {
+
+    super(props)
+
+    this.state = {
+      componentToShow: "welcome"
+    }
+  }
+
+  login = () => {
+    this.setState({ componentToShow: "login" });
+  }
+
+  logout = () => {
+    this.setState({ componentToShow: "welcome" });
+  }
+
+  onLogin = (event, username, password) => {
+    event.preventDefault();
+
+    request(
+      "POST",
+      "/login",
+      { login: username, password: password }
+    ).then(
+      (response) => {
+        this.setState({ componentToShow: "messages" })
+      }
+    ).catch(
+      (error) => {
+        this.setState({ componentToShow: "welcome" })
+      }
+    )
+
+  }
+
+  onRegister = (event, firstName, lastName, username, password) => {
+    event.preventDefault();
+
+    request(
+      "POST",
+      "/register",
+      {
+        firstName: firstName,
+        lastName: lastName,
+        login: username,
+        password: password
+      }
+    ).then(
+      (response) => {
+        this.setState({ componentToShow: "messages" })
+      }
+    ).catch(
+      (error) => {
+        this.setState({ componentToShow: "welcome" })
+      }
+    )
+
+  }
+
+
   render() {
     return (
       <div>
-        <WelcomeContent />
-        <AuthContent />
-        <LoginForm />
+
+        {this.state.componentToShow === "welcome" && <WelcomeContent />}
+        {this.state.componentToShow === "messages" && <AuthContent />}
+        {this.state.componentToShow === "login" && <LoginForm onLogin={this.onLogin} onRegister={this.onRegister} />}
+
       </div>
     );
   }
